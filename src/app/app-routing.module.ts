b89e9e8ec4bba922cb6d4be8bde77a07919bc987
Yupee <gi.velizr@gmail.com>
2019-07-20 04:03:08 -0400

    redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PortafolioComponent } from './portafolio/portafolio.component';
 import { OcioComponent } from './ocio/ocio.component';
 
 const routes: Routes = [
+  {path: '', component: CvComponent },
   {path: 'cv', component: CvComponent },
   {path: 'portafolio', component: PortafolioComponent },
   {path: 'ocio', component: OcioComponent }
