0d944e73be796fa430651aca0f240442da4d4430
Yupee <gi.velizr@gmail.com>
2019-07-20 04:01:56 -0400

    se crea el routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { OcioComponent } from './ocio/ocio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cv', component: CvComponent },
+  {path: 'portafolio', component: PortafolioComponent },
+  {path: 'ocio', component: OcioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
+import { OcioComponent } from './ocio/ocio.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    OcioComponent,
+    PortafolioComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
