77b3947811f421c6720f526a97619b52cd168e45
Giusseppe <giusseppe.veliz@readinessit.com>
2020-03-06 09:47:57 -0300

    se aplican respectivas traducciones

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 export interface Archieve{
   year:string;
@@ -24,7 +25,7 @@ export class CvComponent implements OnInit,OnDestroy {
   ed1: Archieve = {
     year:'2010 - 2014',
     university: 'Duoc UC Viña del mar',
-    title: 'Titulado de Ingeniería en Informática',
+    title: 'main.cv.education.edu1',
     desc: '',
     tipo: 'edu',
     img: 'assets/img/duoc.png',
@@ -34,7 +35,7 @@ export class CvComponent implements OnInit,OnDestroy {
   ed2: Archieve = {
     year:'2018 - 2018',
     university: 'Duoc UC Viña del mar',
-    title: 'Titulado de Ingeniería en Informática',
+    title: 'main.cv.education.edu2',
     desc: '',
     tipo: 'edu',
     img: 'assets/img/duoc.png',
@@ -43,8 +44,8 @@ export class CvComponent implements OnInit,OnDestroy {
   ex1: Archieve = {
     year:'2010 - 2014',
     university: 'Woodtech',
-    title: 'Internship',
-    desc: 'I developed a project called SRS (Segmentation Review System), which allows you to review the segmentation of captures of trucks loaded with wood.',
+    title: 'main.cv.experience.exp1.title',
+    desc: 'main.cv.experience.exp1.desc',
     tipo: 'exp',
     img: 'assets/img/woodtech.webp',
     url: 'http://www.woodtechms.com/'
@@ -52,8 +53,8 @@ export class CvComponent implements OnInit,OnDestroy {
   ex2: Archieve = {
     year:'2014 - 2016',
     university: 'Woodtech',
-    title: 'Junior Software Engineer',
-    desc: 'Team member of OyS (Operations and services), i worked in the continuous improvement of the software delivered to Woodtech\'s customers. My functions were develop software, create reports of log measures for a sawmill located in the USA.',
+    title: 'main.cv.experience.exp2.title',
+    desc: 'main.cv.experience.exp2.desc',
     tipo: 'exp',
     img: 'assets/img/woodtech.webp',
     url:'http://www.woodtechms.com/'
@@ -61,14 +62,14 @@ export class CvComponent implements OnInit,OnDestroy {
   ex3: Archieve = {
     year:'2019 - 2020',
     university: 'Readiness IT',
-    title: 'Trainee',
-    desc: 'During 4 months, i learned HTML5, CSS, GIT, Angular y EOC (Ericsson Order Care) a technology used as Back-End to manage the operations of many external systems, also developed a simple web site simulating a shop with a shopping cart system using Angular. Then, i traveled to Fundao, Portugal for 1 month to learn how operates the development team in Portugal. Currently, im working at the bot team, solving issues that happens at production in the system OneClick.',
+    title: 'main.cv.experience.exp3.title',
+    desc: 'main.cv.experience.exp3.desc',
     tipo: 'exp',
     img: 'https://readinessit.com/wp-content/uploads/2019/06/Logo_ReadinessIT_beyond_digital_experience-01.png',
     url: 'https://readinessit.com/'
   };
 
-  constructor() { }
+  constructor(private translateService: TranslateService) { }
 
   ngOnInit() {
     this.populate();
