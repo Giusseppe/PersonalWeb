bf4b0f60fcbfa227a66b34cb594fad8cf997b867
Yupee <gi.velizr@gmail.com>
2020-03-09 23:59:29 -0300

    se agrega listado de proeyctos en portafolio, falta agregar la info respectiva de cada proyecto,.

--- src/app/portafolio/portafolio.component.ts
+++ src/app/portafolio/portafolio.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Proyecto{
+  idProyecto: string;
+  title: string;
+  desc: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html',
   styleUrls: ['./portafolio.component.css']
 })
+
+
 export class PortafolioComponent implements OnInit {
+
+  proyectos: Proyecto[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.populate();
+  }
+  populate() {
+    this.proyectos.push({idProyecto:'main.portafolio.p1.id',title: 'main.portafolio.p1.title',desc:'main.portafolio.p1.desc',img: 'assets/img/proyects/p1.png'});
+    this.proyectos.push({idProyecto:'main.portafolio.p2.id',title: 'main.portafolio.p2.title',desc:'main.portafolio.p2.desc',img:'assets/img/proyects/p1.png'});
   }
 
 }
